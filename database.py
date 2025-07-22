d7d7a384a186f06f7764b2054b0fbb4b048b2d38
diversv <svetafl627@gmail.com>
2025-07-21 22:16:58 -0700

    Alembic added

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:svetafl@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///./todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- routers/users.py
+++ routers/users.py
@@ -44,11 +44,12 @@ async def change_password(user:user_dependency,db:db_dependency, user_verificati
     if user is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
     user_model = db.query(Users).filter(Users.id == user.get('id')).first()
-    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+    if not bcrypt_context.verify(user_verification.password, user_model.new_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Error on password change")
-    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    user_model.hashed_password = bcrypt_context.encrypt(user_model.new_password)
     db.add(user_model)
     db.commit()
+<<<<<<< HEAD
 
 @router.put("/phone_number/{phone_number}", status_code=status.HTTP_204_NO_CONTENT)
 async def change_phone_number(user:user_dependency,db:db_dependency, phone_number:str):
@@ -58,3 +59,5 @@ async def change_phone_number(user:user_dependency,db:db_dependency, phone_numbe
     user_model.phone_number = phone_number
     db.add(user_model)
     db.commit()
+=======
+>>>>>>> parent of cb4b632 (new route Users)
